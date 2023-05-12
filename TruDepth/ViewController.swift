19e4f20a8278e21b554c1b3b56a69b63121ebea5
Akhash <akhash.shunmugam@gmail.com>
2023-05-13 04:11:54 +0530

    Initial Commit

---
+++ TruDepth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TruDepth
+//
+//  Created by Akhash Subramanian Shunmugam on 5/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
