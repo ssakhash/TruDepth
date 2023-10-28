d395cc703aba922e414d903fdbb353a645b629fa
Akhash Subramanian Shunmugam <akhash.shunmugam@gmail.com>
2023-10-28 00:06:41 -0500

    Comments

--- TruDepth/ViewController.swift
+++ TruDepth/ViewController.swift
@@ -1,30 +1,30 @@
-//MARK: - Import necessary frameworks
-import ARKit
-import AVFoundation
-import UIKit
+/// MARK: - Import necessary frameworks
+import ARKit         // Augmented Reality framework
+import AVFoundation  // Framework for working with audio and video
+import UIKit         // User interface kit for iOS apps
 
-// MARK: - ViewController class definition
+/// This ViewController handles the ARSession to display live camera feed and depth data.
 class ViewController: UIViewController, ARSessionDelegate {
     
-    //MARK: - Outlets
-    //MARK: ImageView to display depth data
+    // MARK: - Outlets
+    /// ImageView to display the depth data.
     @IBOutlet weak var imageView: UIImageView!
-    //MARK: ImageView to display live camera feed
+    
+    /// ImageView to display the live camera feed.
     @IBOutlet weak var liveFeedView: UIImageView!
     
-    //MARK: - Variables
-    //MARK: Instance of ARSession
+    // MARK: - Variables
+    /// Instance of ARSession to manage the augmented reality session.
     var session: ARSession!
     
-    //MARK: - Life Cycle
-    // Set up AR session when view loads
+    // MARK: - Life Cycle
+    /// Set up AR session when the view loads.
     override func viewDidLoad() {
         super.viewDidLoad()
         setupARSession()
     }
     
-    // MARK: - Setup ARSession
-    // Set up ARSession with world tracking and scene depth configuration
+    /// Configures and starts the ARSession with world tracking and scene depth.
     func setupARSession() {
         session = ARSession()
         session.delegate = self
@@ -33,25 +33,26 @@ class ViewController: UIViewController, ARSessionDelegate {
         session.run(configuration)
     }
     
-    // MARK: - ARSession Delegate Methods
-    // When ARSession updates, get camera feed and depth data
+    /// ARSession delegate method called when the session updates.
+    /// - Parameters:
+    ///   - session: The current ARSession.
+    ///   - frame: The current ARFrame.
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
-        // Update live feed view with camera feed
-        updateLiveFeedView(with: frame)
-        
-        // Update image view with depth data
-        updateImageView(with: frame)
+        updateLiveFeedView(with: frame)  // Update the live feed view.
+        updateImageView(with: frame)     // Update the depth data view.
     }
     
-    // Display the camera feed on the liveFeedView
+    /// Updates the liveFeedView with the camera feed.
+    /// - Parameter frame: The current ARFrame.
     private func updateLiveFeedView(with frame: ARFrame) {
         let pixelBuffer = frame.capturedImage
         let ciImage = CIImage(cvPixelBuffer: pixelBuffer).oriented(.right)
         let croppedImage = cropToSquare(image: ciImage)
         liveFeedView.image = UIImage(ciImage: croppedImage)
     }
-
-    // Display the depth data on the imageView
+    
+    /// Updates the imageView with the depth data.
+    /// - Parameter frame: The current ARFrame.
     private func updateImageView(with frame: ARFrame) {
         guard let depthMap = frame.sceneDepth?.depthMap else { return }
         let depthImage = CIImage(cvPixelBuffer: depthMap).oriented(.right)
@@ -61,8 +62,10 @@ class ViewController: UIViewController, ARSessionDelegate {
         let finalImage = drawPoints(on: UIImage(ciImage: croppedDepthImage), points: gridPoints, values: depthValues)
         imageView.image = finalImage
     }
-
-    // Crop the input image to a square
+    
+    /// Crops the input image to form a square.
+    /// - Parameter image: The image to crop.
+    /// - Returns: Cropped square image.
     private func cropToSquare(image: CIImage) -> CIImage {
         let extent = image.extent
         let length = min(extent.width, extent.height)
@@ -71,20 +74,26 @@ class ViewController: UIViewController, ARSessionDelegate {
 
         return image.cropped(to: squareExtent)
     }
-
-    // Generate a grid of points within the given rectangle
+    
+    /// Generates a grid of points within the given rectangle.
+    /// - Parameter extent: The rectangle to generate grid points for.
+    /// - Returns: Array of grid points.
     private func generateGridPoints(for extent: CGRect) -> [CGPoint] {
         var points = [CGPoint]()
         for i in 1...8 {
             for j in 1...8 {
-                let point = CGPoint(x: CGFloat(j) / 9, y: 1 - CGFloat(i) / 9) // Mirrored points
+                let point = CGPoint(x: CGFloat(j) / 9, y: 1 - CGFloat(i) / 9)
                 points.append(point)
             }
         }
         return points
     }
-
-    // Get the depth values for the given points from the pixel buffer
+    
+    /// Gets the depth values for the given points from the pixel buffer.
+    /// - Parameters:
+    ///   - points: Array of grid points.
+    ///   - pixelBuffer: The depth data pixel buffer.
+    /// - Returns: Array of depth values.
     private func depthValues(for points: [CGPoint], in pixelBuffer: CVPixelBuffer) -> [Float] {
         CVPixelBufferLockBaseAddress(pixelBuffer, .readOnly)
         let width = CVPixelBufferGetWidth(pixelBuffer)
@@ -94,22 +103,27 @@ class ViewController: UIViewController, ARSessionDelegate {
         let floatBuffer = unsafeBitCast(CVPixelBufferGetBaseAddress(pixelBuffer), to: UnsafeMutablePointer<Float>.self)
 
         for point in points {
-            let x = Int(point.y * CGFloat(width)) // Adjusted x
-            let y = Int((1 - point.x) * CGFloat(height)) // Adjusted y, now mirrored
+            let x = Int(point.y * CGFloat(width))
+            let y = Int((1 - point.x) * CGFloat(height))
 
             if x < width && y < height {
                 let pixel = floatBuffer[y * width + x]
                 values.append(pixel)
             } else {
-                values.append(-1) // if there's no value or the index is out-of-bound, append -1
+                values.append(-1) // append -1 if index is out-of-bound.
             }
         }
 
         CVPixelBufferUnlockBaseAddress(pixelBuffer, .readOnly)
         return values
     }
-
-    // Draw the points and values on the image
+    
+    /// Draws the points and their corresponding depth values on the image.
+    /// - Parameters:
+    ///   - image: The base image to draw upon.
+    ///   - points: Array of grid points.
+    ///   - values: Array of depth values.
+    /// - Returns: Image with drawn points and depth values.
     private func drawPoints(on image: UIImage, points: [CGPoint], values: [Float]) -> UIImage {
         let renderer = UIGraphicsImageRenderer(size: image.size)
         let newImage = renderer.image { (context) in
@@ -139,7 +153,7 @@ class ViewController: UIViewController, ARSessionDelegate {
                 
                 // Draw the text
                 let valueString = String(format: "%.2f m", values[index])
-                let textPoint = CGPoint(x: x - 5, y: y - 10)  // adjust the y value as needed
+                let textPoint = CGPoint(x: x - 5, y: y - 10)
                 valueString.draw(at: textPoint, withAttributes: textAttributes)
             }
         }
